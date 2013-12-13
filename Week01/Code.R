1a996baa624e8c9b1e599e6d9e28a73340e7e9a5
Dublin R <DublinRUserGroup@gmail.com>
2013-12-13 14:29:32 +0000

    Create Code.R

---
+++ Week01/Code.R
@@ -0,0 +1,17 @@
+######################################################
+#Question 3
+set.seed(31);
+heightsCM = rnorm(30,mean=188, sd=5);
+weightsK = rnorm(30,mean=84,sd=3); 
+hasDaughter = sample(c(TRUE,FALSE),size=30,replace=T); 
+dataFrame = data.frame(heightsCM,weightsK,hasDaughter); 
+dataFrameSubset <- dataFrame[heightsCM > 188, c(1:3)]
+mean(dataFrameSubset$weightsK)
+
+########################################################
+#Question 4
+set.seed(41)
+cauchyValues <- rcauchy(100)
+set.seed(415)
+sample(cauchyValues, size=10, replace=T)
+########################################################
